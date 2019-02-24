0a4fcb17b709641a139f458b054cd19f745ab945
Drake Talley <robertdraketalley@gmail.com>
2019-02-24 18:04:53 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read some TOS some 
+            Read some TOS  
           </a>
         </header>
       </div>
