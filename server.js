0e332e1cdac28ea4345a1fc50a81a9e5a7eec72c
axylos <robertdraketalley@gmail.com>
2019-02-26 08:11:46 -0500

    add nodemon and morgan

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const { app } = require('./app');
 
 const PORT = 8080;
 
+app.use(logger('dev'));
+
 const msg = () => console.log('server up and running', process.env.NODE_ENV);
 
 app.listen(PORT, msg);
