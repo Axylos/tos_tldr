577bec50f03a28173883e67d9cbcb5d08ca7ae9c
drake <=>
2019-02-26 17:52:41 -0500

    hardcode token

--- client/src/services/tos.js
+++ client/src/services/tos.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8080';
+const TOKEN = '305b9d34-e959-4d5c-98a9-1cdefb0fa8d0'
 
 const api = axios.create({
-  baseURL: BASE_URL 
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
 });
 
 const apiTest = async () => {
