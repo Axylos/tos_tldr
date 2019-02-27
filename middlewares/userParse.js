6b6b229bbd2cf4e66cb62b3f5ef4ed5fca4cdfb0
drake <=>
2019-02-26 19:21:22 -0500

    added middleware

--- middlewares/userParse.js
+++ middlewares/userParse.js
@@ -1,13 +1,20 @@
 const userParse = (req, res, next) => {
-  console.log(req.headers);
-  console.log('user header: ', req.headers['x-user-name']);
-  console.log('user header: ', req.headers['x-user-id']);
-  console.log('user header: ', req.headers['x-user-email']);
-  res.locals.user = {
-    name: req.headers['x-user-name'],
-    email: req.headers['x-user-email'],
-    id: req.headers['x-user-id'],
+  let user = {
+    name: 'bobby',
+    email: 'bobby@gmail.com',
+    id: 3,
   };
+
+  if (process.env.NODE_ENV === 'production') {
+    user = {
+      name: req.headers['x-user-name'],
+      email: req.headers['x-user-email'],
+      id: req.headers['x-user-id'],
+    };
+  }
+
+  res.locals.user = user;
+
   next();
 };
 
